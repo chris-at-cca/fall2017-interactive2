c22a64aa73c067bc848fa7aae998dadba164f68c
hamamoto <chamamoto@gmail.com>
2017-09-06 21:52:23 -0700

    Sample 50 charrette

---
+++ exercise-charrette/assets/script.js
@@ -0,0 +1,12 @@
+$(document).ready(function(){
+	
+	$('#button-3').click(function() {
+		$('#button-5').animate({
+			opacity: 0.4,
+			marginLeft: '0.6in',
+			fontSize: '54px',
+			borderWidth: '10px',
+		}, 1000 );
+	});
+
+});
