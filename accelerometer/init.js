6895fb9b98cdd89df68188e04223a6e87ca8e0ed
hamamoto <chamamoto@gmail.com>
2017-10-22 11:11:06 -0700

    Removing comma

--- accelerometer/init.js
+++ accelerometer/init.js
@@ -90,7 +90,7 @@ Graphic.prototype.accelHandler = function(accel){
  var c  = HSVtoRGB(h,1,1);
  this.background_color = "rgb("+c.r+","+c.g+","+c.b+")" ;
  if(this.activated){
-    $("#orientation").html("x: "+x+", h:"+h+,", x*10:"+(x*10));
+    $("#orientation").html("x: "+x+", h:"+h+", x*10:"+(x*10));
     $(".round").css("border-radius", x*10)
     $fun.css("background-color", this.background_color);
  }
